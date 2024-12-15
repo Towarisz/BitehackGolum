118d6c170afc0467838e62d7e304503ae4bebbcf
Mateusz Stoch <matis2k4@gmail.com>
2024-12-15 06:21:38 +0100

    changed respawn save method and gameover screen

--- BHG/Assets/Scripts/CheckpointScript.cs
+++ BHG/Assets/Scripts/CheckpointScript.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 public class checkpointScript : MonoBehaviour
 {
     private CheckpointHandlerScript checkpointHandler;
+    private PlayerMovement playerMovement;
 
     private void Start()
     {
         checkpointHandler = Object.FindFirstObjectByType<CheckpointHandlerScript>();
+        playerMovement = Object.FindFirstObjectByType<PlayerMovement>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag( "Player") && checkpointHandler.lastCheckpoint != (Vector2)transform.position)
         {
-            checkpointHandler.setCheckpoint(transform.position);
+            checkpointHandler.setCheckpoint(transform.position - new Vector3(0,-playerMovement.currentTimeline*playerMovement.gapBetweenTimelines,0f));
             
             Object.Destroy(this);
         }
